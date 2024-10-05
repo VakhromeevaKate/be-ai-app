bd063710097abe79233d43837c983227251e00ee
evakhromeeva <vakhromeevae@gmail.com>
2024-10-05 12:31:54 +0300

    fix

--- dummies/barChartData.ts
+++ dummies/barChartData.ts
@@ -2,7 +2,7 @@ export const waterGlassesData = {
     labels: ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
     datasets: [
       {
-        data: [6, 8, 5, 8, 8, 8, 1]
+        data: [6, 8, 5, 8, 0, 0, 0]
       }
     ]
   };
